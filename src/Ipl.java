5588f0b93a1ef2e9dbcfa651e47a115820869efb
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-10-20 15:06:21 +0530

    Corrected some if else formatting

--- src/Ipl.java
+++ src/Ipl.java
@@ -14,10 +14,12 @@ public class Ipl {
         match = matchesLine.split(",");
         int year = Integer.parseInt(match[1]);
 
-        if(matchesPerYear.containsKey(year))
+        if(matchesPerYear.containsKey(year)) {
             matchesPerYear.put(year, matchesPerYear.get(year) + 1);
-        else
+        }
+        else {
             matchesPerYear.put(year, 1);
+        }
     }
 
     public static void getMatchesWonByAllTeamInAllYear(String matchesLine) {
@@ -25,11 +27,10 @@ public class Ipl {
         match = matchesLine.split(",");
         String winnerTeam = match[10];
 
-        if(!winnerTeam.equals("") && matchesWonByAllTeamInAllYear.containsKey(winnerTeam)){
+        if(!winnerTeam.equals("") && matchesWonByAllTeamInAllYear.containsKey(winnerTeam)) {
             matchesWonByAllTeamInAllYear.put(winnerTeam, matchesWonByAllTeamInAllYear.get(winnerTeam) + 1);
         }
-        else if(!winnerTeam.equals(""))
-        {
+        else if(!winnerTeam.equals("")) {
             matchesWonByAllTeamInAllYear.put(winnerTeam, 1);
         }
     }
@@ -39,12 +40,15 @@ public class Ipl {
         delivery = deliveriesLine.split(",");
         String bowlingTeam = delivery[3];
         int extraRunsConceded = Integer.parseInt(delivery[16]);
+
         if(matchesId.contains(delivery[0]))
         {
-            if(extraRunsConcededIn2016.containsKey(bowlingTeam))
+            if(extraRunsConcededIn2016.containsKey(bowlingTeam)) {
                 extraRunsConcededIn2016.put(bowlingTeam, extraRunsConcededIn2016.get(bowlingTeam) + extraRunsConceded);
-            else
+            }
+            else {
                 extraRunsConcededIn2016.put(bowlingTeam, extraRunsConceded);
+            }
         }
     }
     public static void main(String[] args){
@@ -62,7 +66,7 @@ public class Ipl {
             while((matchesLine = matchesBufferedReader.readLine()) != null) {
                 getMatchesPerYear(matchesLine);
                 getMatchesWonByAllTeamInAllYear(matchesLine);
-                if((matchesLine.split(","))[1].equals("2016")){
+                if((matchesLine.split(","))[1].equals("2016")) {
                     matchId2016.add((matchesLine.split(","))[0]);
                 }
             }
