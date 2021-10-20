5d82727cd03aaa87fe4653386a35e3b0c6d2a0d5
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-10-20 17:14:40 +0530

    Added question message to solution

--- src/Ipl.java
+++ src/Ipl.java
@@ -117,13 +117,17 @@ public class Ipl {
         }
         List<Map.Entry<String,Float>> sortedEconomy = new ArrayList<>(economy.entrySet());
         sortedEconomy.sort((economy1, economy2) -> economy1.getValue().compareTo(economy2.getValue()));
-
+        System.out.println();
+        System.out.println("1. Number of matches played per year of all the years in IPL.");
         System.out.println(matchesPerYear);
         System.out.println();
+        System.out.println("2. Number of matches won of all teams over all the years of IPL.");
         System.out.println(matchesWonByAllTeamInAllYear);
         System.out.println();
+        System.out.println("3. For the year 2016 get the extra runs conceded per team.");
         System.out.println(extraRunsConcededIn2016);
         System.out.println();
+        System.out.println("4. For the year 2015 get the top economical bowlers.");
         System.out.println(sortedEconomy);
 
     }
