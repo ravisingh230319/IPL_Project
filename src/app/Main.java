5dc749952e65018cdee3da40826ff3635af14520
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-10-25 06:29:00 +0530

    Creating Main class and declaring constants

---
+++ src/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+public class Main {
+    private static final int ID = 0;
+    private static final int SEASON = 1;
+    private static final int TOSS_WINNER = 6;
+    private static final int WINNER = 10;
+    private static final int PLAYER_OF_MATCH = 13;
+    private static final int MATCH_ID = 0;
+    private static final int BOWLING_TEAM = 3;
+    private static final int BATSMAN = 6;
+    private static final int BOWLER = 8;
+    private static final int WIDE_RUNS = 10;
+    private static final int NO_BALL_RUNS = 13;
+    private static final int BATSMAN_RUNS = 15;
+    private static final int EXTRA_RUNS = 16;
+    private static final int TOTAL_RUNS = 17;
+    private static final int DISMISSAL_KIND = 19;
+    private static final int FIELDER = 20;
+
+    public static void main(String args[]) {
+    }
+
+}
