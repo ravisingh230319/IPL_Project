73f179adce5d161cd00f736b67f04a41fe3c801e
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-10-25 06:25:20 +0530

    Creating Delivery class using model

---
+++ src/app/Delivery.java
@@ -0,0 +1,103 @@
+package app;
+
+public class Delivery {
+    private int matchId;
+    private String bowlingTeam;
+    private String bowler;
+    private int extraRuns;
+    private String batsman;
+    private int batsmanRuns;
+    private int wideRuns;
+    private int noballRuns;
+    private int totalRuns;
+    private String dismissalKind;
+    private String fielder;
+
+    public int getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(int matchId) {
+        this.matchId = matchId;
+    }
+
+    public String getBowlingTeam() {
+        return bowlingTeam;
+    }
+
+    public void setBowlingTeam(String bowlingTeam) {
+        this.bowlingTeam = bowlingTeam;
+    }
+
+    public String getBowler() {
+        return bowler;
+    }
+
+    public void setBowler(String bowler) {
+        this.bowler = bowler;
+    }
+
+    public int getExtraRuns() {
+        return extraRuns;
+    }
+
+    public void setExtraRuns(int extraRuns) {
+        this.extraRuns = extraRuns;
+    }
+
+    public String getBatsman() {
+        return batsman;
+    }
+
+    public void setBatsman(String batsman) {
+        this.batsman = batsman;
+    }
+
+    public int getBatsmanRuns() {
+        return batsmanRuns;
+    }
+
+    public void setBatsmanRuns(int batsmanRuns) {
+        this.batsmanRuns = batsmanRuns;
+    }
+
+    public int getWideRuns() {
+        return wideRuns;
+    }
+
+    public void setWideRuns(int wideRuns) {
+        this.wideRuns = wideRuns;
+    }
+
+    public int getNoballRuns() {
+        return noballRuns;
+    }
+
+    public void setNoballRuns(int noballRuns) {
+        this.noballRuns = noballRuns;
+    }
+
+    public int getTotalRuns() {
+        return totalRuns;
+    }
+
+    public void setTotalRuns(int totalRuns) {
+        this.totalRuns = totalRuns;
+    }
+
+    public String getDismissalKind() {
+        return dismissalKind;
+    }
+
+    public void setDismissalKind(String dismissalKind) {
+        this.dismissalKind = dismissalKind;
+    }
+
+    public String getFielder() {
+        return fielder;
+    }
+
+    public void setFielder(String fielder) {
+        this.fielder = fielder;
+    }
+}
