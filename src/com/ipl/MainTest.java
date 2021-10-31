de1fbd60ee8ceb972cb2c6ec9c88542f18a4821d
Ravi Kumar Singh <ravi.singh.17@mountblue.tech>
2021-11-01 03:37:00 +0530

    Testing getDeliveriesForAllSeason() method.

--- src/com/ipl/MainTest.java
+++ src/com/ipl/MainTest.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 class MainTest {
     List<Match> matches = Main.getMatchesForAllSeason();
+    List<Delivery> deliveries = Main.getDeliveriesForAllSeasons();
 
     @Test
     void testGetMatchesForAllSeason() {
@@ -17,6 +18,19 @@ class MainTest {
                 () -> assertEquals("Sunrisers Hyderabad", matches.get(0).getWinner()),
                 () -> assertEquals("Royal Challengers Bangalore", matches.get(0).getTossWinner()),
                 () -> assertEquals("Yuvraj Singh", matches.get(0).getPlayerOfMatch())
+        );
+    }
+
+    @Test
+    void testGetDeliveriesForAllSeasons() {
+        assertAll(
+                () -> assertNotNull(deliveries),
+                () -> assertEquals(150460, deliveries.size()),
+                () -> assertEquals(1, deliveries.get(0).getMatchId()),
+                () -> assertEquals("Royal Challengers Bangalore", deliveries.get(0).getBowlingTeam()),
+                () -> assertEquals("TS Mills", deliveries.get(0).getBowler()),
+                () -> assertEquals(0, deliveries.get(0).getExtraRuns()),
+                () -> assertEquals("DA Warner", deliveries.get(0).getBatsman())
         );
     }
 }
